8aa044f15557cc986cdede0201212351ca77c544
choisungmin <76083571+csm5099@users.noreply.github.com>
2022-11-14 06:20:37 +0900

    linked list node insert success 0.02

--- 02_LinkedList.py
+++ 02_LinkedList.py
@@ -1,95 +1,89 @@
-#node class
 class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
     
-#Linked list class  
 class LinkedList:
   def __init__(self):
-    self.no = 0
     self.head = None
     self.tail = None
     self.current = None
-    
+    self.no = 0
+  
   def __len__(self):
     return self.no
-    
+  
   def create_node(self, data):
     return Node(data)
   
-  '''
-  def search_node(self,data):
-    count = 0
-    cursor = self.head
-    while cursor is not None:
-      if cursor.data == data:
-        self.current = cursor
-        return count
-      count +=1
-      cursor = cursor.next
-    return -1
-  
-  def before_cursor(self, index):
-    cursor = self.head
-    for i in range(index):
-      self.current = cursor
-      
-    def is_empty(self):
-    if self.head is None:
-      print("this list is empty!!!")
+  def is_list_empty(self):
+    if self.head == None:
       return True
     else:
-      print("this list is not empty!!!")
       return False
-      
-    def add_node_last(self, data):
-    new = self.create_node(data)
-    if self.tail.next == None:
-      self.tail.next = new
-      self.tail = new
-  '''
-
-  #insert_node
-  def insert_node(self, data):
-    new = self.create_node(data)
-    #빈 리스트인지 확인하기
-    if self.head == None:
-      #그냥 삽입
-      self.head = new
-      self.tail = new
-      pass
-    else:
-      #맨앞 삽입
-      if self.current.next == None:
-        new.next = self.current  
-        self.current = new
-      #중간 삽입
-      else:
-        new.next = self.current.next
-        self.current.next = new
-
+  
+  def add_node_to_empty(self,data):
+    print("empty")
+    new_node = self.create_node(data)
+    self.head = new_node
+    self.tail = new_node
+    self.current = new_node
+    self.no += 1
+  
+  def add_node_to_first(self,data):
+    print("first")
+    new_node = self.create_node(data)
+    new_node.next = self.head
+    self.head = new_node
+    self.no += 1
+  
+  def add_node_to_middle(self,data):
+    print("middle")
+    new_node = self.create_node(data)
+    new_node.next = self.current.next
+    self.current.next = new_node
+    self.no += 1
+  
+  def add_node_to_last(self, data):
+    print("last")
+    new_node = self.create_node(data)
+    self.tail.next = new_node
+    self.tail = new_node
     self.no += 1
     
-  def sort_asc(self, data):
-    tmp = self.head
-    while tmp is not None:
-      if tmp.data > data:
-        self.current = tmp
-        self.insert_node(data)
+  def sort_asc(self,data):
+    cursor = self.head
+    while cursor is not None:
+      if cursor.data > data:
+        return cursor
       else:
-        tmp = tmp.next
-    self.insert_node(data)
-    
+        cursor = cursor.next
+    return None
+  
+  def do_sort_insert(self,data):
+    if self.is_list_empty():
+      self.add_node_to_empty(data)
+    else:
+      cursor = self.sort_asc(data)
+      if cursor == self.head:
+        self.add_node_to_first(data)
+      elif cursor == None:
+        self.add_node_to_last(data)
+      else:
+        self.add_node_to_middle(data)
+
   def print_list(self):
     tmp = self.head
     while tmp is not None:
-      print(tmp.data)
+      print(f"{tmp.data}")
       tmp = tmp.next
-    
+
 lkdlst = LinkedList()
-lkdlst.sort_asc(5)
-lkdlst.sort_asc(1)
-lkdlst.sort_asc(2)
-lkdlst.sort_asc(6)
+lkdlst.do_sort_insert(1)
+lkdlst.print_list()
+lkdlst.do_sort_insert(5)
+lkdlst.print_list()
+lkdlst.do_sort_insert(3)
+lkdlst.print_list()
+lkdlst.do_sort_insert(7)
 lkdlst.print_list()
